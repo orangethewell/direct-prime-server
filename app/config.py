c3f3dcb98a81d55fb7d34702ce897841a94cf700
Wellington Júnior <67232334+orangethewell@users.noreply.github.com>
2024-12-11 11:56:20 -0300

    feat: :sparkles: Add session caching

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
 import os
+from cachelib import FileSystemCache
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_TYPE = 'cachelib'
+    SESSION_SERIALIZATION_FORMAT = 'json'
+    SESSION_CACHELIB = FileSystemCache(threshold=500, cache_dir="/instance/sessions"),
--- app/modules.py
+++ app/modules.py
@@ -1,6 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_session import Session
 from .app import app
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+from sqlalchemy import MetaData
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db, render_as_batch=True)
+session = Session(app)
